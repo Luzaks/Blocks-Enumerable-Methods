97950fcf7c4d203f20f68004f3f4a894a1422541
Luzaks <kanemekanik@gmail.com>
2019-12-03 15:00:06 -0600

    Add the test for my_any? method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -54,4 +54,50 @@ RSpec.describe Enumerable do
       expect(%w[ant bear cat].my_all?(/m/)).to eql(%w[ant bear cat].all?(/m/))
     end
   end
+
+  describe "#my_any?" do
+    it "return true when any of the numbers are greater than 6" do
+      expect([10, 5, 20, 40, 1].my_any? { |x| x > 6 }).to be_truthy
+    end
+
+    it "return false when none of all the numbers are greater than 6" do
+      expect([2, 3, 1, 5, 4].my_all? { |x| x > 6 }).to be_falsy
+    end
+
+    it "return true if its equal as any? method" do
+      expect([10, 20, 20, 40, 50].my_any? { |x| x > 6 }).to eql([10, 20, 20, 40, 50].any? { |x| x > 6 })
+    end
+
+    it "return true if at least one of the elements in an array are true" do
+      expect([nil, true, true].my_any?).to be_truthy
+    end
+
+    it "return false if none of the elements of the array are true" do
+      expect([false, false, false].my_any?).to be_falsy
+    end
+
+    it "return true if its equal as any? method" do
+      expect([nil, true, true].my_any?).to eql([nil, true, true].any?)
+    end
+
+    it "return true if any the elements has the same class" do
+      expect(["hey", "hola", 1].my_any?(String)).to be_truthy
+    end
+
+    it "return true if any the elements has the same class" do
+      expect([13123, "434123", "431321"].my_any?(Integer)).to be_truthy
+    end
+
+    it "return true if is equal to any? method" do
+      expect([13123, nil, 431321].my_any?(Integer)).to eql([13123, nil, 431321].any?(Integer))
+    end
+
+    it "return true if any of the elements contains the same letter" do
+      expect(%w[ant bear cat].my_any?(/n/)).to be_truthy
+    end
+
+    it "return true if is equal to any? method" do
+      expect(%w[ant bear cat].my_any?(/n/)).to eql(%w[ant bear cat].any?(/n/))
+    end
+  end
 end
